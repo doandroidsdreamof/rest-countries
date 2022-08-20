d451e704f0634d056ad5423d63ba070159eddbcf
Berkay <102474166+doandroidsdreamof@users.noreply.github.com>
2022-08-20 05:26:06 +0300

    fixed up button

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
         const upButton = document.querySelector('.scroll-up');
         const upSvg = document.querySelector('.up-svg');
         window.addEventListener('scroll', () => {   
-        window.scrollY > 100 ?  upButton.style.display = "block" &&  upSvg.style.display = "block" : upButton.style.display = "none" &&  upSvg.style.display = "block"
+        window.scrollY > 100 ?  upButton.style.display = "block" &&  upSvg.style.display = "block" : upButton.style.display = "none" &&  upSvg.style.display = "none"
      
           });
 
