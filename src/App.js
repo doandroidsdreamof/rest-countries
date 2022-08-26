0f6f429add29fe2cbd0e35164c2a66659934974a
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-08-27 01:52:32 +0300

    fixed padding of components

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ const res = await fetch(`https://restcountries.com/v3.1/name/${getName}`)
            {countries.slice(0,`${show}`).map((country,index) =>{
              return (
               <main>
-             <div key={country.name} onClick={() => { singleCountries(country,index); setHideInput(!hideInput)}} className={!load ? 'animate-pulse   translate-y-14 shadow-2xl  space-y-4 md:w-eighty space-x-2 rounded-md md:mr-auto md:ml-auto  cursor-pointer relative left-0 h-nintyFive hover:opacity-75 hover:shadow-2xl  bg-lm-elements-white   flex flex-col items-center ' : `${dark ? 'bg-dark-blue' : 'bg-lmelements-white'} translate-y-14 shadow-2xl space-y-4 md:w-eighty space-x-2 rounded-md md:mr-auto md:ml-auto  cursor-pointer relative left-0 h-nintyFive hover:opacity-75 hover:shadow-2xl   object-cover  flex flex-col items-center `} >
+             <div key={country.name} onClick={() => { singleCountries(country,index); setHideInput(!hideInput)}} className={!load ? 'animate-pulse   translate-y-14 shadow-2xl  space-y-4 md:w-eighty space-x-2 rounded-md md:mr-auto md:ml-auto  cursor-pointer relative left-0 h-nintyFive hover:opacity-75 hover:shadow-2xl  bg-lm-elements-white   flex flex-col items-center ' : `${dark ? 'bg-dark-blue' : 'bg-lmelements-white'} translate-y-14 shadow-2xl space-y-4 md:w-eighty space-x-2 rounded-md md:mr-auto md:ml-auto   cursor-pointer relative left-0 h-nintyFive hover:opacity-75 hover:shadow-2xl   object-cover  flex flex-col items-center `} >
              <img className={!load ? 'opacity-0' : "object-cover max-h-36 w-full rounded-t-md"} 
                src={country.flags.png} 
                alt={country.cca3} 
@@ -142,12 +142,12 @@ const res = await fetch(`https://restcountries.com/v3.1/name/${getName}`)
                {/*skeleton animation svg/Puff */}
                <svg class={load ? 'hidden' : `absolute scale-50 sw-full rounded text-gray-200`}xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"></path></svg>
                <Puff className={!load ? " absolute top-1/3 "  : "hidden"}  stroke="black" size={70} strokeOpacity={.125} />
-               <div className={!load ? "opacity-0 w-full p-4" : `w-full p-4`}>
-               <h1  className={`${dark ? 'text-white' : 'text-black' } text-xl pl-1 font-sixHundred`}> {country.name.common}</h1>
-               <h3  className={`${dark ? 'text-white' : 'text-black' } text-base pl-1  pt-1`}><strong>Population:</strong> {new Intl.NumberFormat().format(country.population)}</h3>
-               <h3  className={`${dark ? 'text-white' : 'text-black' } text-base  pl-1 pt-1 `}><strong>Region:</strong> {country.region}</h3>
+               <div className={!load ? "opacity-0 w-full p-4" : `w-full p-3`}>
+               <h1  className={`${dark ? 'text-white' : 'text-black' } text-xl   font-sixHundred`}> {country.name.common}</h1>
+               <h3  className={`${dark ? 'text-white' : 'text-black' } text-base   pt-1`}><strong>Population:</strong> {new Intl.NumberFormat().format(country.population)}</h3>
+               <h3  className={`${dark ? 'text-white' : 'text-black' } text-base   pt-1 `}><strong>Region:</strong> {country.region}</h3>
                {/*if capital is not found return string*/}
-               <h3  className={`${dark ? 'text-white' : 'text-black' } text-base pl-1 pt-1 md:pb-5`  }><strong>Capital:</strong> {country.capital ? country.capital : "None"}</h3>               
+               <h3  className={`${dark ? 'text-white' : 'text-black' } text-base pt-1 pb-3 md:pb-5`  }><strong>Capital:</strong> {country.capital ? country.capital : "None"}</h3>               
                </div>          
            </div>
            </main>
