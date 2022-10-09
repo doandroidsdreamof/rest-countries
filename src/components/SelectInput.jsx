b190e5c592842b97c0b4c3618cfb06f1d0b7d632
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-09 07:35:14 +0300

    removed logs

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 
- const SelectInput = (prop) => {
-
+const SelectInput = (prop) => {
   return (
-    <select onChange={(getRegion) => prop.onChange(getRegion.target.value)} className={`select focus:select-primary w-full max-w-xs md:max-w-sm dark:text-white  dark:bg-dark-blue  bg-white    font-NunitoSans shadow-md rounded-md cursor-pointer`} >
-    <option value="Filter by Region" disabled>Filter by Region</option>
-    <option value="All">All</option>
-    <option value="Africa">Africa</option>
-    <option value="Americas">Americas</option>
-    <option value="Asia">Asia</option>
-    <option value="Europe">Europe</option>
-    <option value="Oceania">Oceania</option>
-  </select>
-
-
-  )
-}
-
+    <select onChange={(getRegion) => prop.onChange(getRegion.target.value)} className={`select focus:select-primary w-full max-w-xs md:max-w-sm dark:text-white  dark:bg-dark-blue  bg-white    font-NunitoSans shadow-md rounded-md cursor-pointer`}>
+      <option value="Filter by Region" disabled>
+        Filter by Region
+      </option>
+      <option value="All">All</option>
+      <option value="Africa">Africa</option>
+      <option value="Americas">Americas</option>
+      <option value="Asia">Asia</option>
+      <option value="Europe">Europe</option>
+      <option value="Oceania">Oceania</option>
+    </select>
+  );
+};
 
 export default SelectInput;
