b026f940eff89be515f82092912ffd98e83a3506
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-11 23:54:19 +0300

    language text bug fixed

--- src/components/details/DetailsContentSecond.jsx
+++ src/components/details/DetailsContentSecond.jsx
@@ -2,76 +2,54 @@ import React, { useEffect, useState } from "react";
 
 const DetailsContentSecond = ({ currencies, domainTop, language, code }) => {
   const [parseLanguage, setParseLanguage] = useState([]);
-  const [parseCurrencies, setParseCurrencies] = useState([]); 
+  const [parseCurrencies, setParseCurrencies] = useState([]);
   useEffect(() => {
     mapItems(language, currencies);
-  }, [language,currencies]);
+  }, [language, currencies]);
 
   const mapItems = (language, currencies) => {
     if (language && currencies === undefined) {
       return;
     }
     if (language === undefined) {
-      for(let x in currencies){
-        setParseCurrencies(currencies[x])
+      for (let x in currencies) {
+        setParseCurrencies(currencies[x]);
       }
-      return
+      return;
     }
     if (currencies === undefined) {
-      for(let x in language){
-        setParseLanguage(language[x])
-
+      for (let x in language) {
+        setParseLanguage(language[x]);
       }
-      return
+      return;
     }
 
     if (language && currencies !== undefined) {
-      for(let x in currencies){
-        setParseCurrencies(currencies[x])
-
+      for (let x in currencies) {
+        setParseCurrencies(currencies[x]);
       }
-      for(let i in language){
-        setParseLanguage(language[i])
-
+      for (let i in language) {
+        setParseLanguage(language[i]);
       }
-
     }
   };
 
-
-
-
-  return <>
-        <p className={`dark:text-gray-300 text-gray-800  text-sm  font-NunitoSans`}>
+  return (
+    <>
+      <p className={`dark:text-gray-300 text-gray-800  text-sm  font-NunitoSans`}>
         Top Level Domain:&nbsp;
         {domainTop && domainTop !== undefined ? domainTop : "none"}
       </p>
       <p className={`dark:text-gray-300   text-gray-800  text-sm font-NunitoSans `}>
         Currencies:&nbsp;
-        {code !== "ATA" || "BVT"  ? parseCurrencies.name : "none"}
-
+        {code !== "ATA" || "BVT" ? parseCurrencies.name : "none"}
       </p>
       <p className={`dark:text-gray-300 text-gray-800   text-sm flex gap-2 flex-wrap font-NunitoSans`}>
         Languages:
-        <span>
-        {code !== "ATA" ||  "BVT"   ?  parseLanguage : "none"}
-        </span>
+        <span>{code !== "ATA" || "BVT" ? parseLanguage : "none"}</span>
       </p>
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  </>;
+    </>
+  );
 };
 
 export default DetailsContentSecond;
