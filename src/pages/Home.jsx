be2fdb6d93ae76e90852c90017d2864e343f49b6
doandroidsdreamof <102474166+doandroidsdreamof@users.noreply.github.com>
2022-10-17 12:16:39 +0300

    pagination bug fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,12 +16,11 @@ const Home = ({ countries, load }) => {
   const [currentCards, setCurrentCards] = useState([]);
   const indexOfLastCard = currentPage * cardsPerPage;
   const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-  
+
   useEffect(() => {
     setCurrentCards(countries);
-    const hidePagination = document.querySelector('.hide-on-search');
-    hidePagination.style.display = 'block';
-
+    const hidePagination = document.querySelector(".hide-on-search");
+    hidePagination.style.display = "block";
   }, [countries]);
 
   const handlePageChange = (page, value) => {
@@ -31,61 +30,52 @@ const Home = ({ countries, load }) => {
     setCurrentPage(currentValue);
   };
   const handleSelect = (e) => {
-    const hidePagination = document.querySelector('.hide-on-search');
+    const hidePagination = document.querySelector(".hide-on-search");
     if (e === "All") {
       setCurrentCards(countries);
-      setCurrentPage(1)
-      hidePagination.style.display = 'block';
-      setCardsPerPage(20)
+      setCurrentPage(1);
+      hidePagination.style.display = "block";
+      setCardsPerPage(20);
       return;
     } else {
       let selectFilter = countries.filter((el, i) => el.region.includes(e));
       setCurrentCards(selectFilter);
-      setCurrentPage(1)
-      setCardsPerPage(selectFilter.length)
-      console.log(selectFilter.length)
-      hidePagination.style.display = 'none';
+      setCurrentPage(1);
+      setCardsPerPage(selectFilter.length);
+      console.log(selectFilter.length);
+      hidePagination.style.display = "none";
     }
   };
 
-
-
-  const searchCountries =   (e) => {
-    let getInput =   e;
-    const hidePagination = document.querySelector('.hide-on-search');
-    hidePagination.style.display = 'none';
-    let findItems =   countries.filter((el,i) =>  el.name.official.toLowerCase().includes(getInput.toLowerCase()))
-    if(findItems.length > 0){
-    let parseArr = Array(cardsPerPage).fill(findItems);
-    setCurrentCards(parseArr[0])
-   }if(getInput.length === 0){
-    hidePagination.style.display = 'block';
-
-
-   }
+  const searchCountries = (e) => {
+    let getInput = e;
+    const hidePagination = document.querySelector(".hide-on-search");
+    hidePagination.style.display = "none";
+    let findItems = countries.filter((el, i) => el.name.official.toLowerCase().includes(getInput.toLowerCase()));
+    if (findItems.length > 0) {
+      let parseArr = Array(cardsPerPage).fill(findItems);
+      setCurrentCards(parseArr[0]);
+    }
+    if (getInput.length === 0) {
+      hidePagination.style.display = "block";
+    }
   };
 
-
-
-
-
-
   return (
-  <main className="bg-lm-bg-gray dark:bg-very-dark-blue overflow-x-hidden">
+    <main className="bg-lm-bg-gray dark:bg-very-dark-blue overflow-x-hidden">
       <div className={` bg-lm-bg-gray dark:bg-very-dark-blue   gap-y-8 flex flex-col  `}>
-      <NavBar />
-      <InputsWrapper select={<SelectInput onChange={handleSelect} />} search={<SearchInput search={searchCountries} />} />
-      <CardGrid skeleton={<Skeleton load={load}  n={cardsPerPage} />} cards={<Cards currentCards={currentCards.slice(indexOfFirstCard, indexOfLastCard)} load={load} />}  />
-      <ScrollUp load={load} />
-      <div className={load ? "block" : "hidden"}>
-        <PaginationWrapper>
-          <Paginate handleChange={handlePageChange} />
-        </PaginationWrapper>
+        <NavBar />
+        <InputsWrapper select={<SelectInput onChange={handleSelect} />} search={<SearchInput search={searchCountries} />} />
+        <CardGrid skeleton={<Skeleton load={load} n={cardsPerPage} />} cards={<Cards currentCards={currentCards.slice(indexOfFirstCard, indexOfLastCard)} load={load} />} />
+        <ScrollUp load={load} />
+        <div className={load ? "block" : "hidden"}>
+          <PaginationWrapper>
+            <Paginate handleChange={handlePageChange} />
+          </PaginationWrapper>
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
   );
 };
 
 export default Home;
-
